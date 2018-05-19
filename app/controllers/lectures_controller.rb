2e4a18e107a1cec70c2ccb2e97f5f57b155da9dd
Amira <miramiromiral@gmail.com>
2018-05-19 12:32:59 +0200

    redirect

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -3,13 +3,15 @@ class LecturesController < InheritedResources::Base
   def upvote 
     @lecture = Lecture.find(params[:id])
     @lecture.upvote_by current_user
-    redirect_to :back
+    #redirect_to :back
+    redirect_back fallback_location: root_path
   end  
   
   def downvote
     @lecture = Lecture.find(params[:id])
     @lecture.downvote_by current_user
-    redirect_to :back
+    #redirect_to :back
+    redirect_back fallback_location: root_path
   end
 
   private
